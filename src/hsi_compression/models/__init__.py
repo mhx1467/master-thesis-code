29b2b04aa97478e502590f049428d3bba0f3fec3
hmadej <contact@hmadej.com>
2026-03-15 21:41:39 +0100

    Add experimenetal TCN v2 that works directly on bands instead of latent representation

--- src/hsi_compression/models/__init__.py
+++ src/hsi_compression/models/__init__.py
@@ -1,9 +1,11 @@
 from .tiny_ae import TinyHSIAutoencoder
 from .baseline_2d_ae import Baseline2DAutoencoder
 from .tcn_ae import TCNHSIAutoencoder
+from .tcn_ae_v2 import TCNHSIAutoencoderV2
 
 __all__ = [
     "TinyHSIAutoencoder",
     "Baseline2DAutoencoder",
     "TCNHSIAutoencoder",
+    "TCNHSIAutoencoderV2",
 ]
