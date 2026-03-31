80458332e3c0bf3c5839f726711476f036b68ea4
hmadej <contact@hmadej.com>
2026-03-31 18:40:58 +0200

    Add new model to init file

--- src/hsi_compression/models/__init__.py
+++ src/hsi_compression/models/__init__.py
@@ -4,6 +4,7 @@ from .baseline_2d_ae import Baseline2DAutoencoder
 from .baseline_3d_ae import Baseline3DAutoencoder
 from .baseline_3d_fullbands_ae import Baseline3DFullBandsAutoencoder
 from .spectral_first_mamba_ae import SpectralFirstMambaAutoencoder
+from .pixelwise_spectral_mamba_ae import PixelwiseSpectralMambaAutoencoder
 from .tcn_ae import TCNHSIAutoencoder
 from .tcn_ae_v2 import TCNHSIAutoencoderV2
 from .tiny_ae import TinyHSIAutoencoder
@@ -16,6 +17,7 @@ __all__ = [
     "Baseline3DAutoencoder",
     "Baseline3DFullBandsAutoencoder",
     "SpectralFirstMambaAutoencoder",
+    "PixelwiseSpectralMambaAutoencoder",
     "TCNHSIAutoencoder",
     "TCNHSIAutoencoderV2",
 ]
